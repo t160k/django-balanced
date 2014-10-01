5cbfedea934b3541dfae733399bed42bd6275023
Thomas Gruner <tom.gruner@gmail.com>
2014-10-01 09:47:06 -0700

    removed automatic sync after post_migrate since it does not work with transactional databases

--- django_balanced/listeners.py
+++ django_balanced/listeners.py
@@ -9,23 +9,9 @@ except ImportError:
     from django.contrib.auth.models import User
     get_user_model = lambda: User
 
-try:
-    post_sync = signals.post_migrate
-except AttributeError:
-    post_sync = signals.post_syncdb
-
-
 # This will create an account per user when they are next saved. Subsequent
 # saves will not make a network call.
 @receiver(signals.post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
     from .models import Account
     Account.objects.get_or_create(user=instance)
-
-
-# Sync with Balanced when the database is synced
-@receiver(post_sync, dispatch_uid='django_balanced.listeners.sync_balanced')
-def sync_balanced(sender, **kwargs):
-    from .models import BankAccount, Credit
-    BankAccount.sync()
-    Credit.sync()
